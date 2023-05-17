e6b15561b4b384c0a22a4c0b05f5a5c8375968ec
Darina Tkachenko <potterpeppi@gmail.com>
2023-05-17 21:11:58 +0300

    add event listener

--- js/main.js
+++ js/main.js
@@ -4,34 +4,26 @@ window.addEventListener("load", (_) => {
         { width: 300, height: 300}
     );
     drawPuck([5,11], [1,5,4,3,6]);
-    function drawPuck(array, array1){
+    function drawPuck(array, filledCellNums){
         let currentCellNumber = 1
         const centerX = canvas.getCenter().x
         const centerY = canvas.getCenter().y
         const cellRad = 15
         const gap = 3 * array.length;
         const cellDiameter = cellRad * 2
-        const rad = cellRad + (gap + cellDiameter * array.length) + 3;
+        const rad = cellRad + (gap + cellDiameter * array.length) + 7;
         console.log(array.length)   
-        canvas.drawCircle(centerX, centerY, rad)
+        canvas.drawFilledCircle(centerX, centerY, rad, 'white')
         for (let rowNumber = 0; rowNumber < array.length; rowNumber++) {      
             for (let cellNumber = 0; cellNumber <  array[rowNumber]; cellNumber++) {
                 const coords = _getCellCoords(rowNumber, array[rowNumber], cellNumber);
-                let isFilled = false;
-
-                for(let a = 0; a < array1.length; a++)   {
-                    if (currentCellNumber == array1[a]){
-                        isFilled = true;
-                    break;
-                    }
-                }
-
+                const isFilled = filledCellNums.includes(currentCellNumber);
+                let cellColor = 'white';
                 if (isFilled){
-                    canvas.drawFilledCircle(coords.x, coords.y, coords.radius, '#21e9ff');
-                } else {
-                    canvas.drawCircle(coords.x, coords.y, coords.radius);
+                    cellColor = '#21e9ff';
                 }
-                canvas.drawText(coords.x, coords.y, currentCellNumber);
+                canvas.drawFilledCircle(coords.x, coords.y, coords.radius, cellColor);
+                canvas.drawText(coords.x, coords.y+3, currentCellNumber);
                 currentCellNumber = currentCellNumber + 1
             }    
        }
@@ -49,7 +41,7 @@ window.addEventListener("load", (_) => {
         const centerY = canvas.getCenter().y
         const cellAngle = _calcCellAngleStep(numberOfCells) * cellNumber;
         const cellRadius = 15;
-        const rowRadius = cellRadius * 2 + 3 + (cellRadius * rowNumber * 2.2) ;
+        const rowRadius = cellRadius * 2 + 3 + (cellRadius * rowNumber * 2.3) ;
         const xCoords = centerX + Math.cos(_degreesToRadians(cellAngle - 90)) * rowRadius;
         const yCoords = centerY + Math.sin(_degreesToRadians(cellAngle - 90)) * rowRadius;
         return new PuckCellCoords(xCoords, yCoords, cellRadius)
