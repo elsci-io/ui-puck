f3f716c605e9ebf61b3edfb50522abf81f53cff6
Kateryna Tkachenko <kateryna.tkachenko@elsci.io>
2023-08-07 13:24:17 +0300

    Changed library name and description of entry points

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,24 @@ import path from 'path';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { fileURLToPath } from 'url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-
+import IgnoreEmitPlugin from 'ignore-emit-webpack-plugin';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-    entry: ['./src/css/style.css', './src/js/index.js'],
+    entry:
+        {
+            // Here entry points are added with the "name" field, and the styles.bundle.css file is added to ignore
+            // If the entry points are simply placed in an array, then will matter the order in which they are written.
+            // And exports will be added only from the last element of the array.
+            'index': './src/js/index.js',
+            'styles': './src/css/style.css'
+        },
     output: {
-        filename: 'index.bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-        library: 'Puck',
+        library: 'elsciPuck',
         libraryTarget: 'umd'
     },
     devtool: "source-map",
@@ -36,7 +43,8 @@ export default {
         new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: 'index.html'
-        })
+        }),
+        new IgnoreEmitPlugin([/styles.*\.js$/])
     ],
     devServer: {
         static: {
